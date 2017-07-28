6814471e81cfc715d5180b5e4cb89ec7026b36f8
Jennifer Fadriquela <jennifer.fadriquela@willistowerswatson.com>
2017-07-28 18:03:42 +0800

    first commit

---
+++ TM.Data/Model/Board.cs
@@ -0,0 +1,8 @@
+﻿namespace TM.Data.Model
+{
+    public partial class Board
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ TM.Data/Model/TMContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TM.Data.Model
+{
+    public partial class TMContext : DbContext
+    {
+        public virtual DbSet<Board> Board { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Data Source=LPC-LR90JUZBS;Initial Catalog=TM.Main;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Board>(entity =>
+            {
+                entity.Property(e => e.Title).IsRequired();
+            });
+        }
+    }
+}
