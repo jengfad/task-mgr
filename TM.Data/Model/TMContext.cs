c0fd2a7f0d21fae05d9ae2424347fc187ab49545
Jennifer Fadriquela <jennifer.fadriquela@willistowerswatson.com>
2017-07-31 10:20:24 +0800

    More code

--- TM.Data/Model/TMContext.cs
+++ TM.Data/Model/TMContext.cs
@@ -6,16 +6,10 @@ namespace TM.Data.Model
 {
     public partial class TMContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public TMContext(DbContextOptions<TMContext> options)
+            : base(options)
         {
-            // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-             optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-7VEDJLR\SQLEXPRESS;Initial Catalog=TM.Main;Integrated Security=True");
         }
-
-        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        // {
-        //     optionsBuilder.UseSqlServer(_connectionString);
-        // }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- TM.Data/Program.cs
+++ TM.Data/Program.cs
@@ -1,30 +1,45 @@
-﻿using System;
-using TM.Data.Model;
-using TM.Data.Repositories;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using TM.Data.Model;
+using TM.Data.Repositories;
 
 namespace TM.Data
 {
     class Program
     {
+        private static IConfigurationRoot Configuration { get; set; }
+
         static void Main(string[] args)
         { 
+            
+            Console.WriteLine("Start Run");
 
-            Console.WriteLine("Starting run");
-            //setup our DI
+#region Configuration Settings
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+#endregion
+
             var services = new ServiceCollection()
-
+            .AddDbContext< TMContext >(opts=>
+            opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")))
             .AddSingleton<IDataAccess< Board, int >, DataAccessRepository>()
             .BuildServiceProvider();
-            Console.WriteLine("Configuring services done");
+
+            Console.WriteLine("Done services config");
 
             var bar = services.GetService<IDataAccess< Board, int >>();
             Console.WriteLine("After configuring bar");
 
-            bar.AddBoard(new Board(){Title = "huhubels"});
-            
-            Console.WriteLine("success");
+            bar.AddBoard(new Board(){Title = "kat edison"});
+
+            Console.WriteLine("end run");
             Console.ReadLine();
         }
 
