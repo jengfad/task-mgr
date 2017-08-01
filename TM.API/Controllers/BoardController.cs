2d2c49db8dd4ed113f9c39571cb0f6c809b555d3
Jennifer Fadriquela <jennifer.fadriquela@willistowerswatson.com>
2017-08-01 13:15:34 +0800

    - Added draft web api

---
+++ TM.API/Controllers/BoardController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TM.Repository.Contracts;
+using TM.Models.Entities;
+
+namespace TM.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class BoardController : Controller
+    {
+        IBaseRepository<Board> _boardRepo;
+        // GET api/board
+        public BoardController(IBaseRepository<Board> b)
+        {
+            _boardRepo = b;
+        }
+
+        [HttpGet]
+        public IQueryable<Board> Get()
+        {
+            var boards = _boardRepo.GetAll();
+
+            return boards;
+        }
+
+        // GET api/board/5
+        [HttpGet("{id}")]
+        public Board Get(int id)
+        {
+            var board = _boardRepo.Get(id);
+            return board;
+        }
+
+        // POST api/board
+        [HttpPost]
+        public void Post([FromBody]Board board)
+        {
+            _boardRepo.Add(board);
+        }
+
+        // PUT api/board/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/board/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
